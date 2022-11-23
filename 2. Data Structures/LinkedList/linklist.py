ee791a647acf492aca25e382b65cc1b15a221291
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:10:07 +0200

    linked class

--- 2. Data Structures/LinkedList/linklist.py
+++ 2. Data Structures/LinkedList/linklist.py
@@ -21,6 +21,49 @@ class Node(object):
 
 
 # the LinkedList class
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        self.count = 0
+
+    def get_count(self):
+        return self.count
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+        self.count += 1
+
+    def find(self, val):
+        item = self.head
+        while (item != None):
+            if item.get_data() == val:
+                return item
+            else:
+                item = item.get_next()
+        return None
+
+    def deleteAt(self, idx):
+        if idx > self.count:
+            return
+        if self.head == None:
+            return
+        else:
+            tempIdx = 0
+            node = self.head
+            while tempIdx < idx-1:
+                node = node.get_next()
+                tempIdx += 1
+            node.set_next(node.get_next().get_next())
+            self.count -= 1
+
+    def dump_list(self):
+        tempnode = self.head
+        while (tempnode != None):
+            print("Node: ", tempnode.get_data())
+            tempnode = tempnode.get_next()
+
 
 # create a linked list and insert some items
 
