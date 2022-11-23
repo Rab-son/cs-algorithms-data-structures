4307255e732e8e4d96a449ed5f437bf5c9340e59
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:12:23 +0200

    queue init and comments

---
+++ 2. Data Structures/Queues/queue.py
@@ -0,0 +1,9 @@
+# try out the Python queue functions
+
+# create a new empty deque object that will function as a queue
+
+# add some items to the queue
+
+# print the queue contents
+
+# pop an item off the front of the queue
