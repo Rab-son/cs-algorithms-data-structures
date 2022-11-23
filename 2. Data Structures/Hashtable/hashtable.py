c6b587e980103b096e02a2e9c4e367e750530440
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:05:55 +0200

    code comments

---
+++ 2. Data Structures/Hashtable/hashtable.py
@@ -0,0 +1,23 @@
+# demonstrate hashtable usage
+
+
+# create a hashtable all at once
+
+
+
+# demonstrate hashtable usage
+
+
+# create a hashtable all at once
+
+
+# try to access a nonexistent key
+# print(items1["key6"])
+
+
+
+# replace an item
+
+
+
+# iterate the keys and values in the dictionary
