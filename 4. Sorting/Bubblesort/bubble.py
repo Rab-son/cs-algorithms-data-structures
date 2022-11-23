1498ae5110105128a7930abf70eda26dfb624c34
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 06:13:07 +0200

    bubble sort function

--- 4. Sorting/Bubblesort/bubble.py
+++ 4. Sorting/Bubblesort/bubble.py
@@ -1,1 +1,13 @@
 # Bubble sort algorithm
+def bubbleSort(dataset):
+    # start with the array length and decrement each time
+    for i in range(len(dataset)-1, 0, -1):
+        # examine each item pair
+        for j in range(i):
+            # swap items if needed
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
+
+        print("Current state: ", dataset)
