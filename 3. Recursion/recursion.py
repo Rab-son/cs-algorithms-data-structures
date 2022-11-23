bd2c1415a66dd578e8b055ac574ed3983cf0bfcc
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:19:36 +0200

    print function

--- 3. Recursion/recursion.py
+++ 3. Recursion/recursion.py
@@ -14,3 +14,8 @@ def factorial(num):
         return 1
     else:
         return num * factorial(num-1)
+
+print("{} to the power of {} is {}".format(5, 3, power(5, 3)))
+print("{} to the power of {} is {}".format(1, 5, power(1, 5)))
+print("{}! is {}".format(4, factorial(4)))
+print("{}! is {}".format(0, factorial(0)))
