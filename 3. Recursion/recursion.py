b334d7fa007a91e7be76ed2b521e91c9be1479e7
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:18:38 +0200

    power function definiton

--- 3. Recursion/recursion.py
+++ 3. Recursion/recursion.py
@@ -1,5 +1,9 @@
 # Using recursion to implement power and factorial functions
 
 
-
-        # breaking condition: if we reach zero, return 1
+def power(num, pwr):
+    # breaking condition: if we reach zero, return 1
+    if pwr == 0:
+        return 1
+    else:
+        return num * power(num, pwr-1)
