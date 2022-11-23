a4ebab5118fc1629aeef7707dc387047490203a9
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:26:36 +0200

    filter comments

---
+++ 6. Other Algorithms/filter.py
@@ -0,0 +1,12 @@
+# use a hashtable to filter out duplicate items
+
+
+# define a set of items that we want to reduce duplicates
+
+
+# create a hashtable to perform a filter
+
+
+# loop over each item and add to the hashtable
+
+# create a set from the resulting keys in the hashtable
