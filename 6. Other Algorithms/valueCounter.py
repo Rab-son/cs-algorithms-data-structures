44f7c4779a5b2961c544928f8cd8b4e2878461d8
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 05:22:13 +0200

    value counter

---
+++ 6. Other Algorithms/valueCounter.py
@@ -0,0 +1,13 @@
+# using a hashtable to count individual items
+
+
+# define a set of items that we want to count
+
+
+# create a hashtable object to hold the items and counts
+
+
+# iterate over each item and increment the count for each one
+
+
+# print the results
