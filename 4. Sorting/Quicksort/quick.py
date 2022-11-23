2b11efd87e8c9fbc8d37f37f9e9a73d2ee847b8e
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-23 06:17:48 +0200

    quick sort

--- 4. Sorting/Quicksort/quick.py
+++ 4. Sorting/Quicksort/quick.py
@@ -1,2 +1,12 @@
 # Implement a quicksort
 items = [20, 6, 8, 53, 56, 23, 87, 41, 49, 19]
+
+
+def quickSort(dataset, first, last):
+    if first < last:
+        # calculate the split point
+        pivotIdx = partition(dataset, first, last)
+
+        # now sort the two partitions
+        quickSort(dataset, first, pivotIdx-1)
+        quickSort(dataset, pivotIdx+1, last)
